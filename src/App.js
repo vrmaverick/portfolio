5482f0bfbd940c0730e4e7e4488ebe5e9455728a
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-10-10 23:03:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
+      <Home />
     </div>
   );
 }
