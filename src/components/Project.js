40595376035acfa26c4e06bc420a14036f9999c7
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-11-04 23:42:20 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,35 +1,101 @@
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
-import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle'; // Import Bootstrap JavaScript
-function UncontrolledExample() {
+import React, { useState } from 'react';
+import { ListGroupItem } from 'react-bootstrap';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled, RxMargin } from 'react-icons/rx';
+import "./Project.css";
+
+
+function App() {
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      content : (
+        <>
+          Project description with a <a href="https://example.com " style={{margin:'0 5px'}}>Read More ...</a>.
+        </>
+      ),
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      content : <>
+      Project description with a <a href="https://example.com " style={{margin:'0 5px'}}>Read More ...</a>.
+    </>
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      content :  <>
+      Project description with a <a href="https://example.com " style={{margin:'0 5px'}}>Read More ...</a>.
+    </>
+    },
+
+    {
+      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+      content :  <>
+      Project description with a <a href="https://example.com " style={{margin:'0 5px'}}>Read More ...</a>.
+    </>
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      content :  <>
+      Project description with a <a href="https://example.com " style={{margin:'0 5px'}}>Read More ...</a>.
+    </>
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentContent, setCurrentContent] = useState(slides[0].content);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+    setCurrentContent(slides[newIndex].content)
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+    setCurrentContent(slides[newIndex].content)
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+    setCurrentContent(slides[slideIndex].content)
+  };
+
   return (
-    <Carousel>
-      <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className='max-w-[1000px] h-[500px] w-full m-auto py-10 px-4 relative group'>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+      {/* Left Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+      <div className='content-container absolute bottom-0 left-0 right-0 text-2xl text-black bg-white p-4'>
+  <div className="center-content duration-500">
+    {currentContent}
+  </div>
+</div>
+    </div>
   );
 }
 
-export default UncontrolledExample;
+export default App;
