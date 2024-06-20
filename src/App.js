34528849baa3069c63f1e461334c57c3f323b02d
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2024-06-20 19:42:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ProjectSlider from './components/Project';
+import About from './components/About'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className="App">
         <Header />
         <ProjectSlider />
+        <About />
         <Footer />
       </div>
     </Router>
