05f4c64826db2669699c109d7a45bdc7c2007cc2
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2024-06-21 17:50:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+//#04364A
+// #176B87
+// #64CCC5
+// #DAFFFB
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProjectSlider from './components/Project';
+import About from './components/About'
+import Card from './components/Card'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <ProjectSlider />
+        <About />
+        <Card />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
